c1fd591319a2d6b94e65f3daf4bf14d2a56f8cf0
Dariel <darielriverabetancourt02@gmail.com>
2025-11-25 13:33:55 -0500

    Agregando la funcionalidad para agregar eventos en una fecha específica

--- calendario.py
+++ calendario.py
@@ -4,9 +4,21 @@ import datetime
 actual_year = datetime.date.today().year
 actual_month = datetime.date.today().month
 
+events_dict = {}
+
 
 def show_month(year: int, month: int):
     print(calendar.month(year, month))
+
+
+def add_event(year, month, day, event):
+    try:
+        date = datetime.datetime(year, month, day)
+    except:
+        print("introduce una fecha válida bro")
+    if not date in events_dict:
+        events_dict[date] = []
+    events_dict[date].append(event)
 
 
 show_month(actual_year, actual_month)
